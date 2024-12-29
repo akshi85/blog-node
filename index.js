388dc033252f7f5a27c90c5ca5a93cac78d64da7
akshi85 <ajudiyaakshita5@gmail.com>
2024-12-29 12:57:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const port = 8080;
+
+const app = express();
+
+const path = require('path')
+
+const database = require('./config/db');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const cookieparser = require('cookie-parser');
+
+app.set('view engine' , 'ejs');
+
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+
+app.use("/images", express.static(path.join(__dirname, "images"))); // Serve images from the images folder
+
+app.use(cookieparser());
+
+app.use(express.urlencoded());
+
+app.use('/',require('./routes/indexRoutes'));
+
+app.listen(port , (err) =>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log(`server is running on :- http://localhost:${port}`);
+})
---
+++ model/travelModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema =new mongoose.Schema({
+      title:{
+        type:String,
+        require:true,
+      },
+
+      content:{
+        type:String,
+        require:true,
+      },
+
+      date:{
+        type:String,
+        require:true,
+      },
+
+      image:{
+        type:String,
+        require:true,
+      },
+});
+
+const travel = mongoose.model('travel',userSchema);
+
+module.exports = travel;
